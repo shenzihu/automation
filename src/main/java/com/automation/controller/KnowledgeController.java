33627a08d81f130b6b20d9d491b0eb10eb36a35d
shenzihu <845282202@qq.com>
2017-11-02 17:07:15 +0800

    添加插入的页面

--- src/main/java/com/automation/controller/KnowledgeController.java
+++ src/main/java/com/automation/controller/KnowledgeController.java
@@ -1,10 +1,11 @@
 package com.automation.controller;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.automation.Result;
 import com.automation.entity.Knowledge;
@@ -12,9 +13,9 @@ import com.automation.entity.KnowledgeRelation;
 import com.automation.repositories.KnowledgeRepository;
 import com.automation.services.KnowledgeService;
 import com.google.common.collect.Lists;
-import com.hazelcast.internal.serialization.impl.ArrayListStreamSerializer;
 
-@RestController("/knowledge")
+@Controller
+@RequestMapping("/")
 public class KnowledgeController {
 
 	@Autowired
@@ -22,7 +23,8 @@ public class KnowledgeController {
 
 	@Autowired
 	KnowledgeRepository repository;
-
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public Result add(@RequestBody KnowledgeRelation relation) {
 		if (relation == null)
 			return Result.error(10001l, "数据为空！");
@@ -56,5 +58,16 @@ public class KnowledgeController {
 			return Result.success("插入成功！");
 		}
 	}
-
+	
+	@RequestMapping(value = "/search")
+	public Result search(String name){
+		if(null == name){
+			return Result.success(repository.findAll());
+		}
+		return Result.success();
+	}
+	@RequestMapping(value = "/a")
+	public ModelAndView index(){
+		return new ModelAndView("index.html");
+	}
 }
