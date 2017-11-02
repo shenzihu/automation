71490ee03d0bca3d0645bc91471d390b209e6e7b
shenzihu <845282202@qq.com>
2017-11-02 17:55:57 +0800

    ceshi

--- src/test/java/com/automation/test/Test1.java
+++ src/test/java/com/automation/test/Test1.java
@@ -4,7 +4,6 @@ import org.neo4j.kernel.api.security.AccessMode.Static;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.err.println(Test1.test());
 	}
 	public static int test() {
 		int a = 0;
