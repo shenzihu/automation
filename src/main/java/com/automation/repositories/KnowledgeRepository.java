da8fd6a946188ef7192b306482c7c3a29b839d50
shenzihu <845282202@qq.com>
2017-11-08 21:43:20 +0800

    写index.html

--- src/main/java/com/automation/repositories/KnowledgeRepository.java
+++ src/main/java/com/automation/repositories/KnowledgeRepository.java
@@ -1,17 +1,22 @@
 package com.automation.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.automation.entity.Knowledge;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "knowledge", path = "knowledge")
 public interface KnowledgeRepository extends
 		PagingAndSortingRepository<Knowledge, Long> {
 
-	Knowledge findByName(String name);
+	Knowledge findByName(@Param("name") String name);
+	
+	//@Query(value = "MATCH (k1:Knowledge)-[c:CONNECTED]-(k2:Knowledge) WHERE k1.name =~ *{name}* return k1,c,k2")
+	Collection<Knowledge> findByNameLike(@Param("name") String name);
 
 	@Query(value = "MATCH (k1),(k2) WHERE k1.name ={name1} AND k2.name = {name2} CREATE (k1)-[:CONNECTED]->(k2)")
 	void addRelations(@Param("name1") String name1, @Param("name2") String name2);
